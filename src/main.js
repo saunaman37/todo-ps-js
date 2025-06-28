cbb1f1e9d3eaf40fee18e255c57550089cb97dd5
saunaman37 <ptiger1015@gmail.com>
2025-06-28 11:12:01 +0900

    テキストボックス処理

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 import './style.css'
 
+const onClickAdd = () =>{
+    //テキストボックスの値を取得し、初期化
+    const inputText = document.getElementById("add-text").value;
+    console.log(inputText);
+    document.getElementById("add-text").value = "";
+}
+
+document.getElementById("add-button").addEventListener("click",onClickAdd);
